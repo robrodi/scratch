9efbaa39438fafb65c2bd75bb2ee68025f604ca5
Rob Rodi <robrodi@microsoft.com>
2012-05-11 15:42:43 -0700

    Kata 1 Done

---
+++ bowling/FSharpSandbox/Module1.fs
@@ -0,0 +1,31 @@
+﻿// Learn more about F# at http://fsharp.net
+
+module Module1
+open System
+open System.Collections.Generic
+open System.Linq
+
+let score(frames: IEnumerable<int[]>) : int =
+    let mutable sum = 0;
+    let framesArray = frames.ToArray()
+    if framesArray.Length > 0 then
+        for i = 0 to Math.Min(framesArray.Length - 1, 9) do
+            let frame = framesArray.[i]
+            let frameSum = if frame.Length = 2 then frame.[0] + frame.[1] else frame.[0]
+            let isSpare = frameSum = 10
+            let isStrike = frame.[0] = 10
+            sum <- sum + frameSum
+
+            let hasNextFrame = (i + 1) < framesArray.Length
+            if isSpare && hasNextFrame then 
+                sum <- sum + framesArray.[i + 1].[0]
+
+            if isStrike && hasNextFrame then 
+                if framesArray.[i + 1].Length = 2 then
+                    sum <- sum + framesArray.[i + 1].[1]
+                elif (i + 2) < framesArray.Length then
+                    sum <- sum + framesArray.[i + 2].[0]
+            
+//            let arg1 = if frame.Length = 2 then frame.[1].ToString() else "X"
+//            Console.WriteLine("{0} {1} {2} {3} {4}", frame.[0], arg1, sum, isSpare, isStrike)
+    sum
---
+++ bowling/FSharpSandbox/Tests.fs
@@ -0,0 +1,58 @@
+﻿module Tests
+
+open NUnit.Framework
+open System.Collections.Generic
+open System.Linq
+open Module1
+
+[<TestFixture>]
+type Awesome() = class
+    let zeroes = [| 0; 0 |]
+
+    [<Test>]
+    member self.SomeTest() = 
+        let i = 1
+        let j = 1
+        Assert.AreEqual(i, j)
+
+    [<Test>]
+    member self.empyGame() =
+        let scores = Enumerable.Repeat(zeroes, 10)
+        let actual = score(scores)
+        Assert.AreEqual(0, actual)
+
+    [<Test>]
+    member self.OnePin() = 
+        let expected = 1;
+        let scores = [| [| 1; 0 |]; zeroes; zeroes; zeroes; zeroes; zeroes; zeroes; zeroes; zeroes; zeroes |]
+        let actual = score(scores)
+        Assert.AreEqual(expected, actual)
+
+    [<Test>]
+    member self.OnePinOnEachRoll() = 
+        let expected = 20;
+        let scores = Enumerable.Repeat([| 1; 1 |], 10)
+        let actual = score(scores)
+        Assert.AreEqual(expected, actual)
+
+    [<Test>]
+    member self.Spare() = 
+        let expected = 15;
+        let scores = [| [| 5; 5 |]; [| 2; 1 |]; zeroes; zeroes; zeroes; zeroes; zeroes; zeroes; zeroes; zeroes; |]
+        let actual = score(scores)
+        Assert.AreEqual(expected, actual)
+
+    [<Test>]
+    member self.Strike() = 
+        let expected = 24;
+        let scores = [| [| 10 |]; [| 3; 4 |]; zeroes; zeroes; zeroes; zeroes; zeroes; zeroes; zeroes; zeroes; |]
+        let actual = score(scores)
+        Assert.AreEqual(expected, actual)
+
+    [<Test>]
+    member self.TwelveStrike() = 
+        let expected = 300;
+        let scores = Enumerable.Repeat([| 10; |], 12)
+        let actual = score(scores)
+        Assert.AreEqual(expected, actual)    
+end
