66086586bbc30ccc6061218b31327729e9ab03ac
Rob Rodi <robert.rodi+git@gmail.com>
2012-05-11 16:28:55 -0700

    basics working

---
+++ StringCalc/StringCalc/Module1.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.net
+module Module1
+open System
+open System.Text.RegularExpressions
+    let numbersRegex = new Regex("^(-?\d+[,\n]? ?)+$")
+
+    let StringCalc(text: string) : int =
+        if String.IsNullOrWhiteSpace(text) then 0
+        else 
+            let m = numbersRegex.Match(text)
+            if (m.Success) then
+                
+                let text = m.Groups.[0].Value
+                let numbers = text.Split([|','; '\n' |], StringSplitOptions.RemoveEmptyEntries)
+                let mutable sum = 0
+                for i in numbers do
+                    Console.WriteLine(">" + i.Trim() + "<")
+                    sum <- sum + Int32.Parse(i.Trim())
+                Console.WriteLine(sum.ToString())
+                sum
+            else
+                Console.WriteLine("Fail")
+                0
---
+++ StringCalc/StringCalc/Tests.fs
@@ -0,0 +1,45 @@
+﻿module Tests
+
+open System
+open NUnit.Framework
+open System.Collections.Generic
+open System.Linq
+open Module1
+
+[<TestFixture>]
+type Awesome() = class
+    [<Test>]
+    member self.firstTest() = 
+        Assert.AreEqual(1,1)
+
+    [<Test>]
+    member self.EmptyStringReturnsZero() = 
+        let expected = 0
+        let actual = StringCalc(String.Empty)
+        Assert.AreEqual(1,1)
+
+    [<Test>]
+    member self.SingleNumberReturnsValue() =
+        let values = Enumerable.Range(-5,5)
+
+        for value in values do
+            Assert.AreEqual(value, StringCalc(value.ToString()))
+    [<Test>]
+    member self.TwoNumbersCommaDelimitedReturnsSum() =
+        let values = [| "1,2"; "1, 2" |]
+        let expected = 3
+        for value in values do
+            Assert.AreEqual(expected, StringCalc(value))
+    [<Test>]
+    member self.TwoNumbersNewlineDelimitedReturnsSum() =
+        let value = "1\n2"
+        let expected = 3
+        Assert.AreEqual(expected, StringCalc(value))
+    
+    [<Test>]
+    member self.ThreeNumbersBothDelimitedReturnsSum() =
+        let value = "1\n2,3"
+        let expected = 6
+        Assert.AreEqual(expected, StringCalc(value))
+        
+end
