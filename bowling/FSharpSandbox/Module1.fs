f50ab3e2d084503bad5ab0e3424f4b42228a758b
Rob Rodi <robert.rodi+git@gmail.com>
2012-05-15 11:28:32 -0700

    all but twelve strike

--- bowling/FSharpSandbox/Module1.fs
+++ bowling/FSharpSandbox/Module1.fs
@@ -3,10 +3,33 @@
 module Module1
 
 let score(frames: int list list) : int =
-    let frameSum (frame : int list) = if frame.Length = 2 then frame.[0] + frame.[1] else frame.[0]
-    let isSpare frame = frameSum frame = 10
-    let isStrike frame = frameSum frame = 10 && frame.Length = 1
+    let isSpare frame = List.sum frame = 10
+    let isStrike frame = List.sum frame = 10 && frame.Length = 1
+    let additionalSpareValue (frame : int list list) = if not frame.Tail.IsEmpty then frame.Tail.Head.Head else 0
+    let additionalStrikeValue (frame : int list list) = 
+        if not frame.Tail.IsEmpty then 
+            if not frame.Tail.Head.Tail.IsEmpty then 
+                frame.Tail.Head.Tail.Head
+            else additionalSpareValue frame.Tail
+        else 0
+    // if the second value in the next frame, otherwise first value in second frame
+    let frameSumX  (frame : int list list) =
+        let frameScore = List.sum frame.Head
+        if isStrike frame.Head then 
+            frameScore + additionalSpareValue frame + additionalStrikeValue frame
+        elif isSpare frame.Head then
+            frameScore + additionalSpareValue frame
+        else
+            frameScore
 
+    let rec frameSum2 (frame : int list list) =
+        if frame.Tail.IsEmpty then
+            frameSumX frame
+        else
+            frameSumX frame  + frameSum2 frame.Tail
+   
+    frameSum2 frames
+    (*
     let frameScores = seq {
         for i = 0 to min (frames.Length - 1)  9 do
             let frame = frames.[i]
@@ -23,7 +46,8 @@ let score(frames: int list list) : int =
                 else
                     0
 
-            yield (frameSum frame) + strikeValue + spareValue
+            yield List.sum frame ::  strikeValue :: spareValue
     }
 
-    Seq.sum frameScores
+//    Seq.sum frameScores
+*)
