d6bd8d8a10e2a103c22afaa0e0db254c5b3bed93
Rob Rodi <robert.rodi+git@gmail.com>
2012-05-15 11:39:44 -0700

    all working with recursion

--- bowling/FSharpSandbox/Module1.fs
+++ bowling/FSharpSandbox/Module1.fs
@@ -2,9 +2,8 @@
 
 module Module1
 
+let maximumFrames = 10
 let score(frames: int list list) : int =
-    let isSpare frame = List.sum frame = 10
-    let isStrike frame = List.sum frame = 10 && frame.Length = 1
     let additionalSpareValue (frame : int list list) = if not frame.Tail.IsEmpty then frame.Tail.Head.Head else 0
     let additionalStrikeValue (frame : int list list) = 
         if not frame.Tail.IsEmpty then 
@@ -12,42 +11,17 @@ let score(frames: int list list) : int =
                 frame.Tail.Head.Tail.Head
             else additionalSpareValue frame.Tail
         else 0
-    // if the second value in the next frame, otherwise first value in second frame
-    let frameSumX  (frame : int list list) =
-        let frameScore = List.sum frame.Head
-        if isStrike frame.Head then 
-            frameScore + additionalSpareValue frame + additionalStrikeValue frame
-        elif isSpare frame.Head then
-            frameScore + additionalSpareValue frame
-        else
-            frameScore
 
-    let rec frameSum2 (frame : int list list) =
-        if frame.Tail.IsEmpty then
-            frameSumX frame
-        else
-            frameSumX frame  + frameSum2 frame.Tail
-   
-    frameSum2 frames
-    (*
-    let frameScores = seq {
-        for i = 0 to min (frames.Length - 1)  9 do
-            let frame = frames.[i]
+    let getFrameScore  (frame : int list list) =
+        if List.sum frame.Head = 10 && frame.Head.Length = 1 then // is strike
+            List.sum frame.Head + additionalSpareValue frame + additionalStrikeValue frame
+        elif List.sum frame.Head = 10 then // is spare
+            List.sum frame.Head + additionalSpareValue frame
+        else List.sum frame.Head
+    
+    let rec sumFrames (frame : int list list, frameNumber) =
+        getFrameScore frame +   if not frame.Tail.IsEmpty && frameNumber < maximumFrames then 
+                                    sumFrames (frame.Tail, (frameNumber + 1)) 
+                                else 0
 
-            let hasNextFrame = (i + 1) < frames.Length
-            let spareValue = if isSpare frame && hasNextFrame then frames.[i + 1].[0] else 0
-            let strikeValue = 
-                if isStrike frame && hasNextFrame then
-                    if frames.[i + 1].Length = 2 then
-                        frames.[i + 1].[1]
-                    elif (i + 2) < frames.Length then
-                        frames.[i + 2].[0]
-                    else 0
-                else
-                    0
-
-            yield List.sum frame ::  strikeValue :: spareValue
-    }
-
-//    Seq.sum frameScores
-*)
+    sumFrames (frames, 1)
